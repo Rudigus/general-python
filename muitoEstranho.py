e9b812214ac0171ff9208ff8e912249c07136586
Rodrigo Matos <rodrigo.matos9@hotmail.com>
2019-06-10 17:37:51 -0300

    Nao é esperamos, é isso mesmo

--- muitoEstranho.py
+++ muitoEstranho.py
@@ -1,39 +1,66 @@
 import pygame
+from random import randint
+import numpy as np
 
 # Programa por Magnus e Rudigus
 
 pygame.init()
 screen = pygame.display.set_mode((620, 620))
+myfont = pygame.font.SysFont("monospace", 30, 1)
+
 done = False
-is_blue = True
-quantMinas = [10, 10]
-minas = []
+is_blue = 0
+quantBlocos = [10, 10]
+blocos = []
+minas = np.zeros((quantBlocos[0], quantBlocos[1]))
+minas[:2,] = 1
+minas = minas.ravel()
+np.random.shuffle(minas)
+minas = minas.reshape(10, 10)
 aux = []
-for i in range(quantMinas[0]):
-    for j in range(quantMinas[1]):
+for i in range(quantBlocos[0]):
+    for j in range(quantBlocos[1]):
         aux.append(pygame.Rect(60 * i + 10, 60 * j + 10, 59, 59))
-    minas.append(aux)
+    blocos.append(aux)
     aux = []
-print(minas)
+blocosAcertados = np.zeros((quantBlocos[0], quantBlocos[1]))
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         posMouse = pygame.mouse.get_pos()
-        print(posMouse)
-       # for i in range(quantMinas[0]):
-           # for j in range(quantMinas[1]):
-               # if(quantMinas[i][j].x == 9999):
-                  #  is_blue = not is_blue
+        for i in range(quantBlocos[0]):
+            for j in range(quantBlocos[1]):
+                if(blocos[i][j].collidepoint(posMouse)):
+                    is_blue = (i,j)
         botoes = pygame.mouse.get_pressed()
-        mouse = botoes[:1]
-        if(all(mouse)):
+        mouse = botoes[0]
+        minaEscolhida = None
+        
+        if(mouse):
+            minaEscolhida = minas[is_blue[0]][is_blue[1]]
             print("Deu certo doido da maconha")
-        if is_blue: color = (0, 128, 255)
-        else: color = (255, 100, 0)
-        for i in range(quantMinas[0]):
-            for j in range(quantMinas[1]):
-                pygame.draw.rect(screen, color, minas[i][j])
+            if(minaEscolhida == 1):
+                print("Booooooooom")
+                done = True
+            else:
+                blocosAcertados[is_blue[0]][is_blue[1]] = 1
+                print("Ahhhh safado")
+        for i in range(quantBlocos[0]):
+            for j in range(quantBlocos[1]):
+                if (blocosAcertados[i][j] == 1):
+                    color = (0, 255, 0)
+                elif not((is_blue) == (i, j)):
+                    color = (0, 128, 255)
+                elif (minaEscolhida == 1):
+                    color = (255, 0, 0)
+                else:
+                    color = (255, 100, 0)
+                pygame.draw.rect(screen, color, blocos[i][j])
+                if (blocosAcertados[i][j] == 1):
+                    label = myfont.render("5", 0, (0, 0, 0))
+                    screen.blit(label, (60 * i + 30, 60 * j + 25))
+        minaEscolhida = None
     pygame.display.flip()
 
